743260647d42bb7a46c27fb5a19ff00f7dcc2a89
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-17 22:17:34 -0300

    feat: add validation to contact us area with javascript

--- js/index.js
+++ js/index.js
@@ -7,5 +7,84 @@ function numeroPedido() {
     outNumeroPedido.textContent = resp;
 }
 
-var btGerarNumeroPedido = document.getElementById("btGerarNumeroPedido");
-btGerarNumeroPedido.addEventListener("load", numeroPedido);
+// var btGerarNumeroPedido = document.getElementById("btGerarNumeroPedido");
+// btGerarNumeroPedido.addEventListener("load", numeroPedido);
+
+
+
+/* Script para validação do contato */ 
+const form = document.getElementById('form-contato');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const cellphoneRegex = /^\s*(\d{2}|\d{0})[-. ]?(\d{5}|)[-. ]?(\d{4})[-. ]?\s*$/;
+const fields = document.querySelectorAll('.field');
+let isError = true;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validarNome();
+    validarTelefone();
+    validarEmail();
+    validarMensagem();
+    if (isError == false) {
+        console.log(isError);
+    form.submit();
+    }
+    
+})
+
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'inline-block';
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function validarNome() {
+    if(campos[0].value.length < 3)
+    {
+        isError = true;
+        setError(0);
+    } else {
+        isError = false;
+        removeError(0);
+    }
+}
+
+function validarTelefone() {
+    if(!cellphoneRegex.test(campos[1].value)) 
+    {
+      isError = true;
+      setError(1);
+    } else {
+      isError = false;
+      removeError(1);
+    }
+  }
+  
+function validarEmail() {
+  if(!emailRegex.test(campos[2].value)) 
+  {
+    isError = true;
+    setError(2);
+  } else {
+    isError = false;
+    removeError(2);
+  }
+}
+
+function validarMensagem() {
+    if(campos[3].value.length < 30)
+    {
+        isError = true;
+        setError(3);
+    } else {
+        isError = false;
+        removeError(3);
+    }
+}
+
