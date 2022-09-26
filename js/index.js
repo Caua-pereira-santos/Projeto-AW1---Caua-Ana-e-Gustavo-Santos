2a0edc0be885014f27040713476dd71d838203b5
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-26 18:58:03 -0300

    feat: adicionado funcao que gera numero de pedido aleatorio

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 /* Confirmação do pedido */
-
 function numeroPedido() {
-    var resp = Math.floor(Math.random() * 10000 + 1);
-
-    var outNumeroPedido  = document.getElementById("outNumeroPedido");
-    outNumeroPedido.textContent = resp;
+  var resp = Math.floor(Math.random() * 10000 + 1);
+  document.getElementById("outNumeroPedido").innerHTML = resp;
 }
 
-// var btGerarNumeroPedido = document.getElementById("btGerarNumeroPedido");
-// btGerarNumeroPedido.addEventListener("load", numeroPedido);
+window.onload = numeroPedido();
 
-
-
-/* Script para validação do contato */ 
+/* Script para validação do contato */
 const form = document.getElementById('form-contato');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
@@ -22,53 +16,50 @@ const fields = document.querySelectorAll('.field');
 let isError = true;
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    validarNome();
-    validarTelefone();
-    validarEmail();
-    validarMensagem();
-    if (isError == false) {
-        console.log(isError);
+  event.preventDefault();
+  validarNome();
+  validarTelefone();
+  validarEmail();
+  validarMensagem();
+  if (isError == false) {
+    console.log(isError);
     form.submit();
-    }
-    
+  }
+
 })
 
 function setError(index) {
-    campos[index].style.border = '2px solid #e63636';
-    spans[index].style.display = 'inline-block';
+  campos[index].style.border = '2px solid #e63636';
+  spans[index].style.display = 'inline-block';
 }
 
 function removeError(index) {
-    campos[index].style.border = '';
-    spans[index].style.display = 'none';
+  campos[index].style.border = '';
+  spans[index].style.display = 'none';
 }
 
 function validarNome() {
-    if(campos[0].value.length < 3)
-    {
-        isError = true;
-        setError(0);
-    } else {
-        isError = false;
-        removeError(0);
-    }
+  if (campos[0].value.length < 3) {
+    isError = true;
+    setError(0);
+  } else {
+    isError = false;
+    removeError(0);
+  }
 }
 
 function validarTelefone() {
-    if(!cellphoneRegex.test(campos[1].value)) 
-    {
-      isError = true;
-      setError(1);
-    } else {
-      isError = false;
-      removeError(1);
-    }
+  if (!cellphoneRegex.test(campos[1].value)) {
+    isError = true;
+    setError(1);
+  } else {
+    isError = false;
+    removeError(1);
   }
-  
+}
+
 function validarEmail() {
-  if(!emailRegex.test(campos[2].value)) 
-  {
+  if (!emailRegex.test(campos[2].value)) {
     isError = true;
     setError(2);
   } else {
@@ -78,13 +69,12 @@ function validarEmail() {
 }
 
 function validarMensagem() {
-    if(campos[3].value.length < 30)
-    {
-        isError = true;
-        setError(3);
-    } else {
-        isError = false;
-        removeError(3);
-    }
+  if (campos[3].value.length < 30) {
+    isError = true;
+    setError(3);
+  } else {
+    isError = false;
+    removeError(3);
+  }
 }
 
