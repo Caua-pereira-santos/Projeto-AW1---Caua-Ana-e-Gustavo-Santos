871f2352e069e6bc85bc0edbc4fc7a7266d36010
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-26 19:26:50 -0300

    feat: colocando validacao para as funcoes so serem carregadas em suas definitivas paginas

--- js/index.js
+++ js/index.js
@@ -1,80 +1,83 @@
 /* Confirmação do pedido */
-function numeroPedido() {
-  var resp = Math.floor(Math.random() * 10000 + 1);
-  document.getElementById("outNumeroPedido").innerHTML = resp;
-}
 
-window.onload = numeroPedido();
+if (document.body.classList.contains('confirmar-pedido')) {
+  window.onload = numeroPedido();
+  function numeroPedido() {
+    var resp = Math.floor(Math.random() * 10000 + 1);
+    document.getElementById("outNumeroPedido").innerHTML = resp;
+  }
+}
 
 /* Script para validação do contato */
-const form = document.getElementById('form-contato');
-const campos = document.querySelectorAll('.required');
-const spans = document.querySelectorAll('.span-required');
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-const cellphoneRegex = /^\s*(\d{2}|\d{0})[-. ]?(\d{5}|)[-. ]?(\d{4})[-. ]?\s*$/;
-const fields = document.querySelectorAll('.field');
-let isError = true;
+if (document.body.classList.contains('contato')) {
+  const form = document.getElementById('form-contato');
+  const campos = document.querySelectorAll('.required');
+  const spans = document.querySelectorAll('.span-required');
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const cellphoneRegex = /^\s*(\d{2}|\d{0})[-. ]?(\d{5}|)[-. ]?(\d{4})[-. ]?\s*$/;
+  const fields = document.querySelectorAll('.field');
+  let isError = true;
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  validarNome();
-  validarTelefone();
-  validarEmail();
-  validarMensagem();
-  if (isError == false) {
-    console.log(isError);
-    form.submit();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validarNome();
+    validarTelefone();
+    validarEmail();
+    validarMensagem();
+    if (isError == false) {
+      console.log(isError);
+      form.submit();
+    }
+
+  })
+
+  function setError(index) {
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'inline-block';
   }
 
-})
+  function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+  }
 
-function setError(index) {
-  campos[index].style.border = '2px solid #e63636';
-  spans[index].style.display = 'inline-block';
-}
+  function validarNome() {
+    if (campos[0].value.length < 3) {
+      isError = true;
+      setError(0);
+    } else {
+      isError = false;
+      removeError(0);
+    }
+  }
 
-function removeError(index) {
-  campos[index].style.border = '';
-  spans[index].style.display = 'none';
-}
+  function validarTelefone() {
+    if (!cellphoneRegex.test(campos[1].value)) {
+      isError = true;
+      setError(1);
+    } else {
+      isError = false;
+      removeError(1);
+    }
+  }
 
-function validarNome() {
-  if (campos[0].value.length < 3) {
-    isError = true;
-    setError(0);
-  } else {
-    isError = false;
-    removeError(0);
+  function validarEmail() {
+    if (!emailRegex.test(campos[2].value)) {
+      isError = true;
+      setError(2);
+    } else {
+      isError = false;
+      removeError(2);
+    }
+  }
+
+  function validarMensagem() {
+    if (campos[3].value.length < 30) {
+      isError = true;
+      setError(3);
+    } else {
+      isError = false;
+      removeError(3);
+    }
   }
 }
-
-function validarTelefone() {
-  if (!cellphoneRegex.test(campos[1].value)) {
-    isError = true;
-    setError(1);
-  } else {
-    isError = false;
-    removeError(1);
-  }
-}
-
-function validarEmail() {
-  if (!emailRegex.test(campos[2].value)) {
-    isError = true;
-    setError(2);
-  } else {
-    isError = false;
-    removeError(2);
-  }
-}
-
-function validarMensagem() {
-  if (campos[3].value.length < 30) {
-    isError = true;
-    setError(3);
-  } else {
-    isError = false;
-    removeError(3);
-  }
-}
-
