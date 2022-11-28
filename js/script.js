82312fb37837104ddd0cf2192452e7ff2e7400e3
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-11-28 16:22:05 -0300

    feat: adicionado tentativa do codigo javascript de pesquisa de cupcakes

--- js/script.js
+++ js/script.js
@@ -189,6 +189,78 @@ hamburguer.addEventListener("click", () => {
 })
 
 
+// const cupcakes = $.getJSON("../cupcakes.json");
+// let cupcakes = 0;
+//  $.getJSON("../cupcakes.json", function(dados){
+//   this.cupcakes = dados;
+// });
+
+// console.log(cupcakes);
+
+/* Código para campo de pesquisa - só funciona se o objeto cupcakes estiver definido nesse documento
+  (retire os comentários do objeto cupcake...) - Gustavo
+*/
+
+const list = document.getElementById('list');
+
+function setList(group) {
+  limparLista();
+
+  for(const cupcake of group) {
+    const item = document.createElement('div');
+    const text = document.createTextNode(cupcake.nome);
+    item.appendChild(text);
+    list.appendChild(item);
+  }
+
+  if(group.length === 0) {
+    naoEncontrado();
+  }
+}
+
+function limparLista() {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+function naoEncontrado() {
+  const item = document.createElement('div');
+  item.classList.add('list-group-item');
+  const text = document.createTextNode('Resultado não encontrado');
+  item.appendChild(text);
+  list.appendChild(item);
+}
+
+
+
+function filtrarPorRelavancia(value, searchTerm) {
+  if (value === searchTerm) {
+    return 2;
+  } else if (value.startsWith(searchTerm)) {
+    return 1;
+  } else if (value.includes(searchTerm)) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('input', (event) => {
+  let value = event.target.value;
+  if (value && value.trim().length > 0) {
+    value = value.trim();
+    setList(cupcakes.filter(cupcake => {
+      return cupcake.nome.includes(value);
+    }).sort((cupcakeA, cupcakeB) => {
+        return filtrarPorRelavancia(cupcakeB.nome, value) - filtrarPorRelavancia(cupcakeA.nome, value);
+    }));
+  } else {
+    limparLista();
+  }
+});
 
 // const search = document.getElementById('search');
 // const matchList = document.getElementById('match-list');
@@ -223,3 +295,75 @@ hamburguer.addEventListener("click", () => {
 // search.addEventListener('input', () => searchStates(search.value));
 
 
+// const cupcakes = [
+//   {
+//       "id": "1",
+//       "nome": "Chocolate Peanut Butter",
+//       "desc": "Cupcake de chocolate com cobertura de doce de leite com chocolate em forma geométrica no topo feito à mão",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado e ovos.",
+//       "alerg": "Contém leite e glúten",
+//       "valEnerg": "145 kcal",
+//       "peso": "30 g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "2",
+//       "nome": "Chocolate Hazelnut",
+//       "desc": "Cupcake de chocolate com cobertura de chocolate e nozes",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado, ovos e nozes.",
+//       "alerg": "Contém leite e glúten",
+//       "valEnerg": "190 kcal",
+//       "peso": "30g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "3",
+//       "nome": "Vanilla Coconut",
+//       "desc": "Cupcake de baunilha com cobertura e raspas de coco",
+//       "ingr": "Açúcar, leite, sacarose, farinha, fermento, água, leite condensado, ovos, baunilha e coco.",
+//       "alerg": "Contém leite e glúten",
+//       "valEnerg": "30 g",
+//       "peso": "30g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "4",
+//       "nome": "Chocolate Coconut",
+//       "desc": "Cupcake de chocolate com cobertura e raspas de coco",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado, ovos e coco.",
+//       "alerg": "Contém leite e glúten",
+//       "valEnerg": "30 g",
+//       "peso": "30g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "5",
+//       "nome": "dia do Orgulho Gay",
+//       "desc": "Cupcake de chocolate caracterizado com bandeira LGBTQIA+",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado e ovos.",
+//       "alerg": "Contém leite",
+//       "valEnerg": "30 g",
+//       "peso": "30g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "6",
+//       "nome": "dia do Jazz",
+//       "desc": "Cupcake de chocolate caracterizado com instrumento saxofone",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado e ovos.",
+//       "alerg": "Contém leite",
+//       "valEnerg": "30 g",
+//       "peso": "30g",
+//       "preco": "R$ 5,00"
+//   },
+//   {
+//       "id": "7",
+//       "nome": "Festival de Filme Internacional de Toronto",
+//       "desc": "Cupcake de chocolate caracterizado com símbolo do teatro/cinema",
+//       "ingr": "Açúcar, leite, sacarose, farinha, chocolate em pó, fermento, água, leite condensado e ovos.",
+//       "alerg": "Contém leite",
+//       "valEnerg": "30 g",
+//       "peso": "30g",
+//       "preco": "R$ 6,00"
+//   }
+// ];
